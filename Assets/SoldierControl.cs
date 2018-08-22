997125b04387274aea0d30609171c291397506af
yyc <delevinyyc@gmail.com>
2018-08-22 19:47:31 +0800

    Initial commit

---
+++ Assets/SoldierControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierControl : MonoBehaviour {
+
+    public float speed;
+
+    private SoldierState _state;
+    private Transform _tf;
+    private Rigidbody _rd;
+	// Use this for initialization
+	void Start () {
+        this._tf = this.gameObject.transform;
+        this._rd = this.gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._rd.MovePosition(this._tf.position + this._tf.forward* speed);
+	}
+}
+
+class SoldierState{
+    public static string IDLE = "idle";
+    public static string WALK = "walk";
+    public static string RUN = "run";
+}
