031c9b35a068ed48557248548ead0a8c7a97a9fd
yyc <delevinyyc@gmail.com>
2018-08-23 15:00:01 +0800

    创建小兵
    *

---
+++ Assets/PopView.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopView : MonoBehaviour {
+
+    public Camera camera;
+    private TextMesh tm;
+    // Use this for initialization
+    void Start()
+    {
+        tm = this.gameObject.GetComponentInChildren<TextMesh>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tm.gameObject.transform.position = this.gameObject.transform.position + new Vector3(0, 2f, 0);
+        tm.gameObject.transform.forward = new Vector3(tm.gameObject.transform.position.x - camera.transform.position.x,
+            tm.gameObject.transform.position.y - camera.gameObject.transform.position.y, tm.transform.position.z - camera.transform.position.z);
+    }
+}
---
+++ Assets/SoldierManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierManager : MonoBehaviour
+{
+    //士兵数量
+    public int s_num = 20;
+    public int max_num = 6;//在场数量
+    public GameObject startGameObject;
+    private int _num = 0;
+    private Transform startPostionT;
+    public static SoldierManager instance;
+    private GameObject _gameObject;
+    public SoldierManager(){
+        
+    }
+    // Use this for initialization
+    void Start()
+    {
+        this._gameObject = GameObject.Find("soldier");
+        this.startPostionT = startGameObject.transform;
+    }
+
+    // Update is called once per frame
+    private float _t = 2;
+    void Update()
+    {
+        _t += Time.deltaTime;
+        if (_t < 2) { return; }else{
+            _t = 0;
+        }
+        if(this._num < this.s_num){
+            GameObject.Instantiate(this._gameObject,startPostionT);
+            this._num++;
+        }
+    }
+
+    //public getInstance():SoldierManager{
+    //    if(SoldierManager.instance == null){
+    //        SoldierManager.instance = new SoldierManager();
+    //    }
+    //    return SoldierManager.instance;
+    //}
+}
